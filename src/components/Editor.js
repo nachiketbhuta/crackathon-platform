c8c93054d238059d7fe7c16b1e7d7d788502816c
Nachiket Bhuta <nachiket.bhuta@somaiya.edu>
2019-09-17 16:21:03 +0000

    Added ace editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,19 +1,51 @@
 import React, { Component, Fragment } from "react";
+import brace from 'brace';
+import AceEditor from 'react-ace';
+import 'brace/mode/java';
+import 'brace/mode/python';
+import 'brace/theme/monokai';
 
-import MonacoEditor from "@uiw/react-monacoeditor";
+import Language from './Editor/Language';
 
 export default class Editor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            code: [],
+            languages: ['C', 'C++', 'Java', 'Python'],
+            isDisabled: false
+        }
+    }
+
+    onChange = (value) => {
+
+    }
+
+    componentDidMount() {
+        this.setState({
+            isDisabled: true
+        })
+    }
+
 	render() {
 		return (
 			<Fragment>
+                <select>
+                    {
+                        this.state.languages.map(lang =>
+                            <Language key={lang} lang={lang} />
+                        )
+                    }
+                </select>
+
 				<div style={{ position: "relative", height: "500px", width: "100%" }}>
-					<MonacoEditor
-						language="javascript"
-						value="<h1>I ♥ react-monacoeditor</h1>"
-						options={{
-							theme: "vs-dark"
-						}}
-					/>
+                    <AceEditor
+                        mode="python"
+                        theme="monokai"
+                        onChange={this.onChange}
+                        name="app"
+                        editorProps={{$blockScrolling: true}}
+                    />
 				</div>
 			</Fragment>
 		);
---
+++ src/components/Editor/Language.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Language(props) {
+    return (
+        <option>{props.lang}</option>
+    );
+}
