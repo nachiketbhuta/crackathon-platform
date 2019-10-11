d56f94b68a1eb380bfa53f6329f2114e7249cf31
hetzz <hetal.kuvadia@somaiya.edu>
2019-10-12 01:33:10 +0530

    Added final component

---
+++ src/components/Final.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Final() {
+    return (
+        <div>
+            Final
+        </div>
+    )
+}
--- src/steps.js
+++ src/steps.js
@@ -4,14 +4,17 @@ import Editor1 from './components/editors/Editor1';
 import Editor2 from './components/editors/Editor2';
 import Editor3 from './components/editors/Editor3';
 import Editor4 from './components/editors/Editor4';
+import Final from './components/Final';
 
 // import Test from './components/Test';
+
 
 const steps = [
     {name: 'Program 1', component: <Editor1/>},
     {name: 'Program 2', component: <Editor2/>},
     {name: 'Program 3', component: <Editor3/>},
     {name: 'Program 4', component: <Editor4/>},
+    {name: 'Final Submission', component: <Final/>},
   ];
 
 export { steps }
